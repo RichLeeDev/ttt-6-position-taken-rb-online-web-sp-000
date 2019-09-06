cee93401fb862b68ae8a493097ddc0641635ede5
Richard Lee <richlee81192@gmail.com>
2019-09-06 07:03:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 require 'pry'# code your #position_taken? method here!
 def position_taken?(array, index)
   binding.pry
-  # if array[index] = 'X' || array[index] = 'O'
+  # if array[index] == 'X' || array[index] == 'O'
     # true
     # elsif array[index] == "nil"
     # false
